e876a92fe7bb1911d6111ebc95b41d2d148b02d0
peterkomo <mwangipeterkomo@gmail.com>
2023-07-05 08:45:21 +0300

    debugged the whole code and made nessesary changes

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-function moviesTheater() {
-    // Fetch movie data from the specified URL an api
-    fetch("http://localhost:3000/films")
-    .then((res) => res.json())
-    .then((arts) => {
-        // Get the HTML element with id "films" under ul element
-        const flatdango = document.getElementById("films");
+// Fetch movie data from the specified URL using an API
+fetch("http://localhost:3000/films")
+  .then((res) => res.json())
+  .then((arts) => {
+    // Get the HTML element with id "films" under ul element
+    const flatdango = document.getElementById("films");
 
-        // Iterate over each movie
-        arts.forEach((movie) => {
-            // Create a new list item
-            const list = document.createElement("li");
-            flatdango.appendChild(list);
+    // Iterate over each movie
+    arts.forEach((movie) => {
+      // Create a new list item
+      const list = document.createElement("li");
+      flatdango.appendChild(list);
 
-            // Create an anchor element
-            const anchor = document.createElement("a");
-            anchor.href = "http://localhost:3000/films";
-            list.appendChild(anchor);
+      // Create an anchor element
+      const anchor = document.createElement("a");
+      anchor.href = `http://localhost:3000/films/${movie.id}`; // Use movie ID to create a specific URL
+      list.appendChild(anchor);
 
-            // Set the text content of the anchor with movie title and ID
-            anchor.textContent = `${movie.title}, ${movie.id}`;
+      // Set the text content of the anchor with movie title and ID
+      anchor.textContent = `${movie.title}, ${movie.id}`;
 
-            // Add an event listener to the anchor
-            anchor.addEventListener("click", (e) => {
-                // Prevent the default behavior of the anchor click event
-                e.preventDefault();
+      // Add an event listener to the anchor
+      anchor.addEventListener("click", (e) => {
+        // Prevent the default behavior of the anchor click event
+        e.preventDefault();
 
-                // Call the displayMovieDetails function with the movie object
-                displayMovieDetails(movie);
-            });
-        });
+        // Call the displayMovieDetails function with the movie object
+        displayMovieDetails(movie);
+      });
     });
-}
+  });
+
+// Function to display movie details
 function displayMovieDetails(movie) {
-    // Set the source of the "poster" image element to the movie's poster URL
-    document.querySelector("#poster").src = movie.poster;
+  // Set the source of the "poster" image element to the movie's poster URL
+  document.querySelector("#poster").src = movie.poster;
 
-    // Set the text content of the "title" element to the movie's title
-    document.querySelector("#title").textContent = movie.title;
+  // Set the text content of the "title" element to the movie's title
+  document.querySelector("#title").textContent = movie.title;
 
-    // Set the text content of the "runtime" element to the movie's runtime in minutes
-    document.querySelector("#runtime").textContent = `Runtime: ${movie.runtime}min`;
+  // Set the text content of the "runtime" element to the movie's runtime in minutes
+  document.querySelector("#runtime").textContent = `Runtime: ${movie.runtime}min`;
 
-    // Set the text content of the "showtime" element to the movie's showtime
-    document.querySelector("#showtime").textContent = `Showtime: ${movie.showtime}`;
+  // Set the text content of the "showtime" element to the movie's showtime
+  document.querySelector("#showtime").textContent = `Showtime: ${movie.showtime}`;
 
-    // Set the text content of the "description" element to the movie's description
-    document.querySelector("#description").textContent = movie.description;
-}
-// Get the capacity of the movie theater
-const availability = movie.capacity;
+  // Set the text content of the "description" element to the movie's description
+  document.querySelector("#description").textContent = movie.description;
 
-// Get the number of tickets already purchased for the movie
-const ticketsPurchased = movie.tickets_sold;
+  // Get the capacity of the movie theater
+  const availability = movie.capacity;
 
-// Calculate the number of available tickets for the movie
-let availableTickets = availability - ticketsPurchased;
+  // Get the number of tickets already purchased for the movie
+  const ticketsPurchased = movie.tickets_sold;
 
-// Get the HTML element with id "availableTickets"
-const availableTicketsElement = document.querySelector("#availableTickets");
+  // Calculate the number of available tickets for the movie
+  let availableTickets = availability - ticketsPurchased;
 
-// Get the HTML element with id "buyTickets"
-const ticketButton = document.querySelector("#buyTickets");
+  // Get the HTML element with id "availableTickets"
+  const availableTicketsElement = document.querySelector("#availableTickets");
 
-// Check if there are available tickets
-if (availableTickets > 0) {
+  // Get the HTML element with id "buyTickets"
+  const ticketButton = document.querySelector("#buyTickets");
+
+  // Check if there are available tickets
+  if (availableTickets > 0) {
     // Update the text content of the "availableTicketsElement" with the available tickets count
     availableTicketsElement.textContent = `Available Tickets: ${availableTickets}`;
 
@@ -72,7 +72,7 @@ if (availableTickets > 0) {
 
     // Enable the ticket button
     ticketButton.disabled = false;
-} else {
+  } else {
     // Update the text content of the "availableTicketsElement" to indicate that tickets are sold out
     availableTicketsElement.textContent = "SOLD OUT";
 
@@ -81,34 +81,38 @@ if (availableTickets > 0) {
 
     // Disable the ticket button
     ticketButton.disabled = true;
-}
-// Add a click event listener to the ticketButton
-ticketButton.addEventListener("click", (am) => {
+  }
+
+  // Add a click event listener to the ticketButton
+  ticketButton.addEventListener("click", (e) => {
     // Prevent the default behavior of the click event
-    am.preventDefault();
+    e.preventDefault();
 
     // Check if there are available tickets
     if (availableTickets > 0) {
-        // Decrement the availableTickets count
-        availableTickets--;
+      // Decrement the availableTickets count
+      availableTickets--;
 
-        // Update the text content of the "availableTicketsElement" with the updated available tickets count
-        availableTicketsElement.textContent = `Available Tickets: ${availableTickets}`;
+      // Update the text content of the "availableTicketsElement" with the updated available tickets count
+      availableTicketsElement.textContent = `Available Tickets: ${availableTickets}`;
 
-        // Check if the availableTickets count reaches 0
-        if (availableTickets === 0) {
-            // Update the text content of the "ticketButton" to indicate "Sold Out"
-            ticketButton.textContent = "Sold Out";
+      // Check if the availableTickets count reaches 0
+      if (availableTickets === 0) {
+        // Update the text content of the "ticketButton" to indicate "Sold Out"
+        ticketButton.textContent = "Sold Out";
 
-            // Disable the ticket button
-            ticketButton.disabled = true;
-        }
+        // Disable the ticket button
+        ticketButton.disabled = true;
+      }
     }
-});
-// Defined  the init function
-function init() {
-    // Call the moviesTheater function
-    moviesTheater();
+  });
 }
-// Added a DOMContentLoaded event listener to the document
+
+// Initialize the code when the DOM content is loaded
+function init() {
+  // Call the moviesTheater function
+  moviesTheater();
+}
+
+// Add a DOMContentLoaded event listener to the document
 document.addEventListener("DOMContentLoaded", init);
